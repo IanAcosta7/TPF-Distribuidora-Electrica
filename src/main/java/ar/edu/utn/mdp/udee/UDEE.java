43ffe6b82c2a5ca55b0fe22aa98a208187538caa
IanAcosta7 <ianacosta700@gmail.com>
2021-03-28 15:20:22 -0300

    Initialized Project.

---
+++ src/main/java/ar/edu/utn/mdp/udee/UDEE.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.mdp.udee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UDEE {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UDEE.class, args);
+	}
+
+}
